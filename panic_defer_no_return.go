f35b3b3be684691b083edad855b30b385910de84
linguang cheng <chenglinguang123@163.com>
2020-02-15 07:04:03 +0000

    use panic and defer without return for a message

---
+++ panic_defer_no_return.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := returnN()
+	fmt.Println(a)
+}
+
+func returnN() (result int) {
+	defer func() {
+		if p := recover(); p != nil {
+			result = p.(int)
+		}
+	}()
+	panic(3)
+}
